1b2f9dcae8455dec8f1e46715c44cf771de8a142
Jana Del Rosario <delrosariojana9@gmail.com>
2026-05-03 21:51:24 +0800

    removed joint 2

--- robot_manual.ino
+++ robot_manual.ino
@@ -3,19 +3,16 @@
 // --- SERVO PINS ---
 #define BASE_PIN 13
 #define JOINT1_PIN 12
-#define JOINT2_PIN 11
 #define CLAMP_PIN 10
 
 // --- SERVO OBJECTS ---
 Servo baseServo;
 Servo joint1Servo;
-Servo joint2Servo;
 Servo clampServo;
 
 // --- CURRENT POSITIONS ---
 int basePos = 90;
 int j1Pos = 90;
-int j2Pos = 90;
 int clampPos = 0;
 
 void setup() {
@@ -24,19 +21,17 @@ void setup() {
   // Attach servos to pins
   baseServo.attach(BASE_PIN);
   joint1Servo.attach(JOINT1_PIN);
-  joint2Servo.attach(JOINT2_PIN);
   clampServo.attach(CLAMP_PIN);
 
   // Set to user-defined defaults
   baseServo.write(basePos);
   joint1Servo.write(j1Pos);
-  joint2Servo.write(j2Pos);
   clampServo.write(clampPos);
 
   // Print instructions
   Serial.println("--- Incremental Calibration Mode ---");
   Serial.println("Commands: [Letter][Number]");
-  Serial.println("b = Base | 1 = Joint 1 | 2 = Joint 2 | c = Clamp");
+  Serial.println("b = Base | 1 = Joint 1 | c = Clamp");
   Serial.println("Examples: b10 (Base +10 deg) | 1-5 (Joint1 -5 deg)");
   Serial.println("------------------------------------");
 }
@@ -68,12 +63,6 @@ void loop() {
       joint1Servo.write(j1Pos);
       Serial.print("Joint 1 adjusted to: ");
       Serial.println(j1Pos);
-    } 
-    else if (cmd == '2') {
-      j2Pos = constrain(j2Pos + amount, 0, 180);
-      joint2Servo.write(j2Pos);
-      Serial.print("Joint 2 adjusted to: ");
-      Serial.println(j2Pos);
     } 
     else if (cmd == 'c' || cmd == 'C') {
       clampPos = constrain(clampPos + amount, 0, 180);
